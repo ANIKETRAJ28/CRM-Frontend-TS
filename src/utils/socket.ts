397b3f1cef5c58f26a1b7c52992e51aa33645e35
Aniket <aniketraj28042003@gmail.com>
2025-04-06 16:06:22 +0530

    fix: added backend url for web socet connection

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,7 +1,7 @@
 import { backendUrl } from "@/config/envConfig";
 
 export function connectSocket(id: string) {
-  const url = "wss:" + backendUrl.split(":")[1];
+  const url = backendUrl.split("://")[1];
   const socket = new WebSocket(`wss://${url}?userId=${id}`);
   return socket;
 }
